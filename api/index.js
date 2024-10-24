b8373edda98a08cf719c5f1b29a5c1b5bfefd89d
Mihai-Elisei <mihai.elisei@outlook.com>
2024-10-24 16:13:29 +0300

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
+import { error } from "console";
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app = express()
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Database is connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+const app = express();
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000!!")
-})
+  console.log("Server is running on port 3000!!");
+});
