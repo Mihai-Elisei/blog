dbdebe45d6064f98c9fe03437242dd0f8fcb5c59
Mihai-Elisei <mihai.elisei@outlook.com>
2024-10-28 16:17:27 +0200

    create get posts api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -35,3 +35,55 @@ export const create = async (req, res, next) => {
     next(errorHandler(500, error.message)); // Handle errors during post save and pass to error handler
   }
 };
+
+// Controller function to retrieve posts based on various query parameters
+export const getPosts = async (req, res, next) => {
+  try {
+    // Extract pagination and sorting parameters from the request query
+    const startIndex = parseInt(req.query.startIndex) || 0; // Start index for pagination (default is 0)
+    const limit = parseInt(req.query.limit) || 9; // Limit for number of posts per page (default is 9)
+    const sortDirection = req.query.order === "asc" ? 1 : -1; // Determine sort order: ascending (1) or descending (-1)
+
+    // Build the filter criteria for fetching posts, including dynamic conditions based on query parameters
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }), // Filter by userId if provided
+      ...(req.query.category && { category: req.query.category }), // Filter by category if provided
+      ...(req.query.slug && { slug: req.query.slug }), // Filter by slug if provided
+      ...(req.query.postId && { _id: req.query.postId }), // Filter by postId if provided
+      ...(req.query.searchTerm && {
+        // Search in title and content if searchTerm is provided
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } }, // Case-insensitive search in title
+          { content: { $regex: req.query.searchTerm, $options: "i" } } // Case-insensitive search in content
+        ]
+      })
+    })
+      .sort({ updatedAt: sortDirection }) // Sort posts by the updatedAt field
+      .skip(startIndex) // Skip the first 'startIndex' posts for pagination
+      .limit(limit); // Limit the result to 'limit' number of posts
+
+    // Count total number of posts in the database (could also consider using relevant filters)
+    const totalPosts = await Post.countDocuments();
+
+    // Get the current date
+    const now = new Date();
+
+    // Create a date object for one month ago
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    // Fetch posts that were created in the last month
+    const lastMonthPosts = await Post.find({
+      createdAt: { $gte: oneMonthAgo } // Filter for posts created since one month ago
+    });
+
+    // Respond with status 200 and the retrieved data: posts, total post count, and last month posts
+    res.status(200).json({ posts, totalPosts, lastMonthPosts });
+  } catch (error) {
+    // Handle any errors that occur within the try block
+    next(errorHandler(500, error.message)); // Call the next middleware with an error
+  }
+};
